d4827500d9b805ac9754306b64d5adc310589c34
Andrew M <matveev.andrew@internet.ru>
2023-01-31 21:54:08 +0600

    HW_Ex13 done

---
+++ Homework2/HW_Задача_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+// Например:
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.Write("Insert the number: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+if (Math.Abs(num) > 99)
+{
+    while (Math.Abs(num) > 1000) num = num / 10;
+    System.Console.WriteLine("Thrid number is: " + (num % 10));
+}
+else System.Console.WriteLine("Inserted number does not have third number");
